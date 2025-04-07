23c47746515b9d13165f75387e7e0e4ed3b88ce2
BeniAnalyticss <bhardoy@mail.austral.edu.ar>
2025-04-07 11:56:40 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,11 +1,3 @@
 def slice_advanced():
-    texto = "Awesome"
-
-    primeras_3 = texto[:3].lower()
-    print(primeras_3)
-    letras_en_medio = texto[2:5].lower()
-    print(letras_en_medio)
-
-    letras_rango_1 = texto[:4].lower()
-    letras_rango_2 = texto[-3:].lower()
-    print(letras_rango_1 + letras_rango_2)
+   txt = input("Ingrese texto: ")
+    print(txt[4::2]
